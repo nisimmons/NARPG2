0bd86460e924776f33810c5106bad5d6da14757a
Nate Simmons <nisimmons01@gmail.com>
2022-04-15 10:38:35 -0500

    input changes

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 //up to date
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         String s;
         Player p;
